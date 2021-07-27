c584eabda55944fdde0b656b78afb19dbc92ce36
play.js <git@gituser.com>
2021-07-27 11:24:10 -0400

    i am cool

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
           >
             <h3>Deploy &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              vercel pog
             </p>
           </a>
         </div>
