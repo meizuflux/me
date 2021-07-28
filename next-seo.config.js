86d16bb1fccf183d82a5b391053b421be3f22eb9
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-07-28 16:29:48 -0400

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,15 +2,15 @@ const title = 'meizuflux'
 const description = 'Personal portfolio for meizuflux'
 
 const SEO = {
-    title,
-    description,
+    title: title,
+    description: description,
     canonical: 'https://meizuflux.pages.dev',
     openGraph: {
         type: 'website',
         locale: 'en_IE',
         url: 'https://meizuflux.pages.dev',
-        title,
-        description,
+        title: title,
+        description: description,
         images: [
             {
                 url: 'https://i.imgflip.com/415gxp.jpg',
