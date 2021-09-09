8318f58b05160d51fd2161856092d8ddbff6c14b
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-09-08 20:37:41 -0400

    ahhhhhh

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import { ColorModeScript } from "@chakra-ui/react"
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
 
 
@@ -7,6 +8,7 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head />
         <body>
+          <ColorModeScript initialColorMode="dark" />
           <Main />
           <NextScript />
         </body>
